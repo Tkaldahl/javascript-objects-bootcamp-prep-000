57ceb4752ae980c366e6600608eef926b11be165
Taylor Kaldahl <tkaldahl90@gmail.com>
2018-03-27 18:27:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,13 +1,10 @@
-var playlist = {coldplay:'clocks'};
+var playlist = {artistName: 'songTitle'}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName] = songTitle;
-  return playlist;
+  return Object.assign({}, playlist, {[artistName]: songTitle})
 }
 
 function removeFromPlaylist(playlist, artistName){
- playlist = {artistName: 'Gold Digger'}
- delete playlist.artistName;
- return playlist
+  delete playlist[artistName];
+  return playlist
 }
-
